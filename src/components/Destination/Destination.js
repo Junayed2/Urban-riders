0885c8316be815b5afd07a320dddf50597bf36b8
Junayed <muhammadjunayed2424@gmail.com>
2021-03-21 22:25:08 +0600

    added new

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -5,18 +5,21 @@ import Car from "../images/Frame-2.png";
 import Bus from "../images/Frame-1.png";
 import Train from "../images/Group.png";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Map from "../images/Map.png";
+import Map from "../GoogleMap/Map";
+
 const Destination = () => {
   const { name } = useParams();
   return (
     <div>
       {/* <h1>This is destination page</h1> */}
+      <div className='col-md-4'>
       <h1>Thank You For Choosing '{name}'</h1>
       {name === "Bike" ? <img src={Bike} alt="" /> : ""}
       {name === "Car" ? <img src={Car} alt="" /> : ""}
       {name === "Bus" ? <img src={Bus} alt="" /> : ""}
       {name === "Train" ? <img src={Train} alt="" /> : ""}
       <h4>Please Select your destination.Where you want to go.</h4>
+      </div>
       <br />
       <div className="row">
         <div className="col-md-4">
@@ -66,8 +69,8 @@ const Destination = () => {
             </form>
           </div>
         </div>
-        <div className="col-md-8">
-          <img src={Map} alt="" />
+        <div  className="col-md-8">
+        <Map></Map>
         </div>
       </div>
     </div>
